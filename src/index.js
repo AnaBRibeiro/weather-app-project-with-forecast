6a7f00fe7b3f03e50ccba69c2651ee251a240298
Ana Ribeiro <anabrazribeiro@gmail.com>
2022-05-03 11:27:35 +0100

    API integration for weather forecast info

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function displayDayAndTime(timestamp) {
+function getCurrentDayAndTime(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
@@ -22,12 +22,69 @@ function displayDayAndTime(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function getForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return day;
+}
+
+function displayForecastData(response) {
+  let weatherForecastDay = response.data.daily;
+  let weatherForecastElement = document.querySelector("#weather-forecast");
+  let weatherForecastHTML = `<div class="row">`;
+  weatherForecastDay.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      weatherForecastHTML =
+        weatherForecastHTML +
+        `<div class="col-2">
+                <div class="day">${getForecastDay(forecastDay.dt)}</div>
+                <div class="weather-forecast-icon">
+                <img
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""/>
+                </div>
+                <div>
+                  <span class="value-temperature-forecast-max">${Math.round(
+                    forecastDay.temp.max
+                  )}º</span>
+                  <span class="value-temperature-forecast-min">${Math.round(
+                    forecastDay.temp.min
+                  )}º</span>
+                </div>
+              </div>`;
+    }
+  });
+  weatherForecastHTML = weatherForecastHTML + `</div>`;
+  weatherForecastElement.innerHTML = weatherForecastHTML;
+}
+
+function makeApiCallKnowingCoordenates(coordinates) {
+  apiKey = `6e2e6ba4445c2a3f7c96186354d36ba3`;
+  let lat = coordinates.lat;
+  let lon = coordinates.lon;
+  let unit = `metric`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=${unit}`;
+  axios.get(apiUrl).then(displayForecastData);
+}
+
 function displayCurrentData(response) {
   let cityNameElement = document.querySelector("#city-name");
   cityNameElement.innerHTML = response.data.name;
 
   let dayAndTimeElement = document.querySelector("#day-and-time");
-  dayAndTimeElement.innerHTML = displayDayAndTime(response.data.dt * 1000);
+  dayAndTimeElement.innerHTML = getCurrentDayAndTime(response.data.dt * 1000);
 
   let weatherDescriptionElement = document.querySelector(
     "#weather-description"
@@ -55,9 +112,11 @@ function displayCurrentData(response) {
 
   let temperatureElement = document.querySelector("#value-temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  makeApiCallKnowingCoordenates(response.data.coord);
 }
 
-function makeApiCall(city) {
+function makeApiCallKnowingCity(city) {
   let apiKey = `6e2e6ba4445c2a3f7c96186354d36ba3`;
   let unit = `metric`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=${unit}`;
@@ -68,31 +127,10 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#input-city");
   let typedCity = cityInputElement.value;
-  makeApiCall(typedCity);
-}
-
-function displayForecastData() {
-  let weatherForecastElement = document.querySelector("#weather-forecast");
-  let weatherForecastHTML = `<div class="row">`;
-  let days = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday"];
-  days.forEach(function (day) {
-    weatherForecastHTML =
-      weatherForecastHTML +
-      `<div class="col-2">
-                <div class="day">${day}</div>
-                <div class="weather-forecast-icon">☁</div>
-                <div>
-                  <span class="value-temperature-forecast-max">12º</span>
-                  <span class="value-temperature-forecast-min">7º</span>
-                </div>
-              </div>`;
-  });
-  weatherForecastHTML = weatherForecastHTML + `</div>`;
-  weatherForecastElement.innerHTML = weatherForecastHTML;
+  makeApiCallKnowingCity(typedCity);
 }
 
 let formElement = document.querySelector("#search-form");
 formElement.addEventListener("submit", handleSubmit);
 
-makeApiCall(`New York`);
-displayForecastData();
+makeApiCallKnowingCity(`New York`);
